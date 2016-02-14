800360a2480718dbaba67852bdac2c4b94de7434
pxm3974 <pranilmaharjan@gmail.com>
2016-02-14 21:39:48 +0000

    heroku update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+    def index
+        @content_first = "This is the main index"
+        @content_second = "this is the same"
+    end
+    
+    def create
+        
+    end
+    def edit
+        
+    end
+    def update
+        
+    end
+    def show
+        
+    end
+    def destroy
+        
+    end
+    
+end
