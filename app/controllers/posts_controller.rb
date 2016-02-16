8cb7a32366ae746b2767eb51a968753122c58a0d
pxm3974 <pranilmaharjan@gmail.com>
2016-02-16 16:38:37 +0000

    initial commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,34 @@
 class PostsController < ApplicationController
     def index
-        @content_first = "This is the main index"
-        @content_second = "this is the same"
+       @posts = Post.all
+    end
+    
+    def new
+        @post=Post.new
+        @category= Category.all
     end
     
     def create
-        
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path, :notice => "Your post was saved"
+        else
+            render "new"
+        end
     end
+    def post_params
+        params.require(:post).permit(:title, :body, :category)
+    end
+   
     def edit
+         @post =Post.find(params[:id])
         
     end
     def update
         
     end
     def show
+        @post =Post.find(params[:id])
         
     end
     def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214213253) do
+ActiveRecord::Schema.define(version: 20160214213734) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "category_id"
+    t.integer  "autor_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
